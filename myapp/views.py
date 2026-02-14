b8a8191b9cb68cac33871866895750a874c1d331
hibajabin <hibajabinkfy@gmail.com>
2026-02-14 10:56:54 -0800

    first commit

---
+++ myapp/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from .models import task
+from .forms import TaskForm
+
+def index(request):
+    tasks = task.objects.all().order_by('-created')
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+
+    return render(request, 'index.html', {
+        'tasks': tasks,
+        'form': form
+    })
+
+def complete_task(request, pk):
+    tasks = task.objects.get(id=pk)
+    tasks.completed = True
+    tasks.save()
+    return redirect('index')
+
+def delete_task(request, pk):
+    tasks = task.objects.get(id=pk)
+    tasks.delete()
+    return redirect('index')
